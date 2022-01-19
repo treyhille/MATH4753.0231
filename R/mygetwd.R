a24e3022e07ab5bb965026e29c429c3996be48db
Trey Hille <treyhille@Treys-MacBook-Pro.local>
2022-01-19 11:16:49 -0600

    Initial start

---
+++ R/mygetwd.R
@@ -0,0 +1,11 @@
+#' @title       This finds the working directory
+#'
+#' @description This is where we put more stuff.
+#'
+#' @return      The working directory.
+#' @export
+#'
+#' @examples    mygetwd()
+mygetwd <- function(){
+  getwd()
+}
